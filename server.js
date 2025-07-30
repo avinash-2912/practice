ab3965315cfc41d48da24f6a2b16bf1888b4753f
atharvamahabal003 <atharvamahabal003@gmail.com>
2025-07-30 06:13:19 +0000

    Changed Dunia

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/json' });
-  res.end(JSON.stringify({message:"Hello world ! kaisa chal rha hai3"}));
+  res.end(JSON.stringify({message:"Hello Dunia ! kaisa chal rha hai3"}));
 });
 
 server.listen(PORT, () => {
